cd01e2be78987518e0bdb44b8dfdae97d75e0fe4
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-08-22 16:07:52 +0200

    Database and ORM initialized

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const sqlite3 = require("sqlite3").verbose();
+const typeorm = require("typeorm");
+
+const db = new sqlite3.Database("wildersdb");
+
+const dataSource = new typeorm.DataSource({
+  type: "sqlite",
+  database: "wildersdb",
+  synchronize: true,
+  entities: [],
+});
