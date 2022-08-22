cdfaa7f030f72266556948360b7c9ebb9529ca88
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-08-22 17:14:40 +0200

    Added Skill

---
+++ src/entity/Skill.js
@@ -0,0 +1,18 @@
+const EntitySchema = require("typeorm").EntitySchema;
+
+module.exports = new EntitySchema({
+  name: "Skill",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true,
+    },
+    name: {
+      type: "text",
+    },
+    votes: {
+      type: "int",
+    },
+  },
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const typeorm = require("typeorm");
 
 const Wilder = require("./entity/Wilder");
+const Skill = require("./entity/Skill");
 
 const dataSource = new typeorm.DataSource({
   type: "sqlite",
   database: "./wildersdb.sqlite",
   synchronize: true,
-  entities: [Wilder],
+  entities: [Wilder, Skill],
 });
 dataSource.initialize().then(() => {
   dataSource.getRepository(Wilder).save({ name: "Jane Doe" });
